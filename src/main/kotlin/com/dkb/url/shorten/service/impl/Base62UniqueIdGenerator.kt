6320c906567f4f3c6ab0fc7a7358781e60dd84c3
kathiresan <kathiresan.in@gmail.com>
2022-05-23 09:14:02 +0530

    Added algorithm to generate unique identifier.

---
+++ src/main/kotlin/com/dkb/url/shorten/service/impl/Base62UniqueIdGenerator.kt
@@ -0,0 +1,20 @@
+package com.dkb.url.shorten.service.impl
+
+import java.util.Random
+
+class Base62UniqueIdGenerator(
+    private val base62Chars : String = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    private val bound : Int = 999999,
+    private val random: Random = Random()
+) {
+    fun generateShortUrlByRandomNumber() : String {
+        val shortUrl : StringBuilder = StringBuilder()
+        var uniqueIdentifier : Int = random.nextInt(bound)
+        while (uniqueIdentifier > 0) {
+            val digit = uniqueIdentifier % 10
+            uniqueIdentifier /= 10
+            shortUrl.append(base62Chars.get(digit))
+        }
+        return shortUrl.reverse().toString()
+    }
+}
