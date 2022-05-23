feae1848c09656ace3a737db58c8c10f568ccd81
kathiresan <kathiresan.in@gmail.com>
2022-05-23 11:45:47 +0530

    Created base version of shorten url rest api.

--- src/main/kotlin/com/dkb/url/shorten/controller/UrlShorteningController.kt
+++ src/main/kotlin/com/dkb/url/shorten/controller/UrlShorteningController.kt
@@ -1,10 +1,38 @@
 package com.dkb.url.shorten.controller
 
+import com.dkb.url.shorten.model.ShortUrl
+import com.dkb.url.shorten.model.ShortUrlRequest
+import com.dkb.url.shorten.service.UrlShorteningService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.Optional
 
 @RestController
 @RequestMapping("/dbk/api")
-class UrlShorteningController {
+class UrlShorteningController(
+    private val urlShorteningService: UrlShorteningService
+) {
+    @PostMapping("/urlshortening")
+    fun generateShortUrl(@RequestBody shortUrlRequest: ShortUrlRequest) : ResponseEntity<ShortUrl> {
+        val shortUrl : Optional<ShortUrl> = urlShorteningService.generateShortUrl(shortUrlRequest)
+        if(shortUrl.isPresent) {
+            return ResponseEntity<ShortUrl>(shortUrl.get(), HttpStatus.CREATED)
+        }
+        return ResponseEntity<ShortUrl>(HttpStatus.INTERNAL_SERVER_ERROR)
+    }
 
+    @GetMapping("/urlshortening/{identifier}")
+    fun resolveOriginalUrlById(@PathVariable identifier: String) : ResponseEntity<ShortUrl> {
+        val shortUrl : Optional<ShortUrl> = urlShorteningService.resolveOriginalUrlById(identifier)
+        if(shortUrl.isPresent) {
+            return ResponseEntity<ShortUrl>(shortUrl.get(), HttpStatus.OK)
+        }
+        return ResponseEntity<ShortUrl>(HttpStatus.NOT_FOUND)
+    }
 }
--- src/main/kotlin/com/dkb/url/shorten/service/impl/UrlShorteningServiceImpl.kt
+++ src/main/kotlin/com/dkb/url/shorten/service/impl/UrlShorteningServiceImpl.kt
@@ -2,15 +2,30 @@ package com.dkb.url.shorten.service.impl
 
 import com.dkb.url.shorten.model.ShortUrl
 import com.dkb.url.shorten.model.ShortUrlRequest
+import com.dkb.url.shorten.repository.ShortUrlRepository
 import com.dkb.url.shorten.service.UrlShorteningService
+import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+import java.util.Optional
 
-class UrlShorteningServiceImpl : UrlShorteningService {
+@Service
+class UrlShorteningServiceImpl (
+    private val shortUrlRepository: ShortUrlRepository,
+    private val base62UniqueIdGenerator: Base62UniqueIdGenerator
+) : UrlShorteningService {
 
-    override fun generateShortUrl(shortUrlRequest: ShortUrlRequest): ShortUrl {
-        TODO("Not yet implemented")
+    override fun generateShortUrl(shortUrlRequest: ShortUrlRequest): Optional<ShortUrl> {
+        var identifier : String = ""
+        do {
+            identifier = base62UniqueIdGenerator.generateShortUrlByRandomNumber();
+            val shortUrlOp : Optional<ShortUrl> = shortUrlRepository.findById(identifier)
+        } while (shortUrlOp.isPresent)
+        val shortUrl : ShortUrl = ShortUrl(identifier, shortUrlRequest.original_url, LocalDateTime.now().toString())
+        shortUrlRepository.save(shortUrl)
+        return Optional.ofNullable(shortUrl)
     }
 
-    override fun resolveOriginalUrlById(identifier: String): ShortUrl {
-        TODO("Not yet implemented")
+    override fun resolveOriginalUrlById(identifier: String): Optional<ShortUrl> {
+        return shortUrlRepository.findById(identifier)
     }
 }
